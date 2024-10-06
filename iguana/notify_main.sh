55a350618e16cdf62cffb2660b3799eeae971091
gcharang <21151592+gcharang@users.noreply.github.com>
2024-10-07 02:41:05 +0530

    adds scripts

---
+++ iguana/notify_main.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# $1 - coin, $2 - blockhash
+curl -s --url "http://127.0.0.1:7776" --data "{\"agent\":\"dpow\",\"method\":\"updatechaintip\",\"blockhash\":\"$2\",\"symbol\":\"$1\"}"
